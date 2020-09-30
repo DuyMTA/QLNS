ed7f32175f46b930e3ed083519fb7e3bde84d551
DuyMTA <boycac0456@gmail.com>
2020-09-30 11:42:40 +0700

    Duy set up environment for the first time

---
+++ QL_NHANSU/QL_NHANSU/Main.cs
@@ -0,0 +1,67 @@
+﻿using QL_NHANSU.Data;
+using QL_NHANSU.GUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QL_NHANSU
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login login = new Login();
+            login.Visible=true;
+        }
+
+        private void hệThốngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangky_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Dangky dangky = new Dangky();
+            dangky.Visible = true;
+        }
+
+        private void btnDangxuat_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void NhanVienToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AddNV add = new AddNV();
+            add.Visible = true;
+        }
+
+        private void DuAn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            DuAnNV da = new DuAnNV();
+            da.Visible = true;
+        }
+
+        private void phòngBanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            PB pb = new PB();
+            pb.Visible = true;
+        }
+    }
+}
